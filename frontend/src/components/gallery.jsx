9c63f5c33b5d0ff95bddc35a54b4bd93e12bb829
Gian-Luca Fenocchi <lucabod8@gmail.com>
2023-02-05 23:02:09 +0000

    ReactJS - frontend

---
+++ frontend/src/components/gallery.jsx
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Gallery</h2>
+          <p>
+            Take a closer look at our smart parking solution through our image gallery, showcasing the technology and features that make it a cutting-edge solution for efficient and convenient parking management.
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                <div
+                  key={`${d.title}-${i}`}
+                  className="col-sm-6 col-md-4 col-lg-4"
+                >
+                  <Image
+                    title={d.title}
+                    largeImage={d.largeImage}
+                    smallImage={d.smallImage}
+                  />
+                </div>
+              ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a href="https://www.youtube.com/watch?v=eBGIQ7ZuuiU" className="btn btn-custom btn-lg page-scroll">
+                  View Parking Spots
+                </a>
+                &nbsp; &nbsp; &nbsp;
+                <a href="#features" className="btn btn-custom btn-lg page-scroll">
+                  Learn More
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
