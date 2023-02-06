2a736db97168fe942a3209ae98a47aae1e0eff52
Gian-Luca Fenocchi <lucabod8@gmail.com>
2023-02-06 16:06:09 +0000

    Update oled_rfid.py

--- oled_rfid.py
+++ oled_rfid.py
@@ -1,5 +1,6 @@
 import board
 import busio
+import RPi.GPIO as GPIO
 from digitalio import DigitalInOut
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
@@ -8,30 +9,44 @@ from adafruit_pn532.i2c import PN532_I2C
 import time
 from datetime import datetime
 
+# LEDs
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+red_led = 17
+green_led = 27
+high = GPIO.HIGH
+low = GPIO.LOW
 
-# RFID setup
+# RFID
 i2cRFID = busio.I2C(board.SCL, board.SDA)
 reset_pin = DigitalInOut(board.D6)
 req_pin = DigitalInOut(board.D12)
 pn532 = PN532_I2C(i2cRFID, debug=False, reset=reset_pin, req=req_pin)
+
+
 # Configure PN532 to communicate with MiFare cards
 pn532.SAM_configuration()
 
 
-# Display setup
+# Display
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial, rotate=0)
 
-# Text for display
 title1 = 'Welcome!'
 title2 = 'Please scan ID...'
 title3 = 'ACCEPTED'
 title4 = 'PROCEED'
 titleIN = 'Status: IN'
 
-# Checks if ID card is already signed in
-def checkSignIn(uid):
+UID_title = 'ID: '
 
+def led(colour, status):
+    GPIO.output(colour, status)
+
+def checkSignIn(uid):
+    # Checks if signing in / out
     with open('status.txt', 'r') as file:
         content = file.read()
         file.close()
@@ -44,14 +59,13 @@ def checkSignIn(uid):
             file.write(new_content)
             file.close()
         return False
-    # Else add ID to status.txt
+    # Else add ID too status.txt
     else:
         print('Signed In')
         with open('status.txt', 'a') as file:
             file.write(uid)
             file.close()
         return True
-
 
 def main():
 
@@ -62,10 +76,10 @@ def main():
     
     statusFile = open('status.txt', 'a')
     
+    led(red_led, high)
+    
     while True:
         now = datetime.now().strftime("%H:%M")
-        
-        # Idle screen
         with canvas(device) as draw:
             draw.rectangle(device.bounding_box, outline="white", fill="black")
             draw.text((90, 8), now, fill="white")
@@ -78,19 +92,25 @@ def main():
         # Try again if no card is available.
         if uid is None:
             continue
-            
+        
+        # If card is detected:
+        # Set Green LED ON and Red LED OFF
+        led(green_led, high)
+        led(red_led, low)
+        
+        # Convert User ID into string format
         uid = [hex(i) for i in uid]
         uid_string = ''.join(format(int(i, 16), '02x') for i in uid)
         print("UID:", uid_string)
         
-        # Accepted screen
+        # Accepted Display
         with canvas(device) as draw:
             draw.rectangle(device.bounding_box, outline="white", fill="black")
             draw.text((90, 8), now, fill="white")
             draw.text((40, 25), title3, fill="white")
         time.sleep(0.5)
         
-        # Accpeted + Proceed screen
+        # Accepted + Proceed Display
         with canvas(device) as draw:
             draw.rectangle(device.bounding_box, outline="white", fill="black")
             draw.text((90, 8), now, fill="white")
@@ -98,16 +118,18 @@ def main():
             draw.text((44, 35), title4, fill="white")
         time.sleep(1)
         
-        # Get ID status
+        # Checks Status IN/OUT
         if checkSignIn(uid_string) == True:
             titleIN = "Status: IN"
         else:
             titleIN = "Status: OUT"
         
-        # Status screen
+        # Status IN/OUT Displayed
         with canvas(device) as draw:
             draw.rectangle(device.bounding_box, outline="white", fill="black")
             draw.text((90, 8), now, fill="white")
-            draw.text((36, 30), titleIN , fill="white")
+            draw.text((33, 30), titleIN , fill="white")
         time.sleep(2)
+        led(green_led, low)
+        led(red_led, high)
 main()
