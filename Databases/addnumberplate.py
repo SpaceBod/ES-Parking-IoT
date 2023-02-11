fa19d6d0ed42ca5ede61703b59b3bee4cf12cbb1
Savraj Sian <94938371+SavrajSian@users.noreply.github.com>
2023-02-11 18:26:46 +0000

    Update addnumberplate.py

--- Databases/addnumberplate.py
+++ Databases/addnumberplate.py
@@ -1,5 +1,19 @@
 import re
 
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
+db = "https://embedded-systems-cf93d-default-rtdb.europe-west1.firebasedatabase.app/"
+keyfile = "embedded-systems-cf93d-firebase-adminsdk-amky8-e0a50b80ba.json"
+scopes = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/firebase.database"
+]
+
+credentials = service_account.Credentials.from_service_account_file(keyfile, scopes=scopes)
+
+authed_session = AuthorizedSession(credentials)
+
+
 def addnumberplate(filename, numberplate):
     
     filename = filename[:-4] #take out .jpg
@@ -21,7 +35,7 @@ def addnumberplate(filename, numberplate):
     for i, obj in enumerate(resp):
         update[i] = obj
 
-    newresp = requests.patch(db+path, json=update)
+    newresp = authed_session.patch(db+path, json=update)
 
     if newresp.ok:
         print("added number plate to database")
