a8654ea80aff40200fc4ede38f9d81ae09ff81fe
Luca <lucabod8@gmail.com>
2023-02-16 21:49:19 +0000

    Updated File System

---
+++ Marketing Website/src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Technologies</h2>
+          <p>
+            Take a closer look inside...
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+              <div key={`${d.name}-${i}`} className="col-md-4">
+                {" "}
+                <i className={d.icon}></i>
+                <div className="service-desc">
+                  <h3>{d.name}</h3>
+                  <p>{d.text}</p>
+                </div>
+              </div>
+            ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
